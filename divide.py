2bb41b07b693fdc1f1c781555e47f1cede94303c
Tom Seccull <tom.seccull@proton.me>
2024-05-09 21:12:31 +0100

    =minor additions to divide.py

--- divide.py
+++ divide.py
@@ -1,8 +1,8 @@
 #! /home/tom/anaconda3/envs/work/bin/python
 """
-divide.py - written by Tom Seccull, 2024-05-08 - v0.0.0
+divide.py - written by Tom Seccull, 2024-05-08 - v0.0.1
 
-	Last updated: 2024-05-08
+	Last updated: 2024-05-09
 	
 	This script divides one 1D spectrum by another. divide.py expects
 	both spectra to have a common wavelength axis, be the product
@@ -35,9 +35,11 @@ parser.add_argument("-s", "--save", action="store_true",
 )
 args = parser.parse_args()
 
-two_hdus = []
-
 with fits.open(args.spec_file_one) as one:
 	one_hdu_list = [x for x in one]
 
-[print(x) for x in one_hdu_list]
+with fits.open(args.spec_file_two) as two:
+	two_hdu_list = [x for x in two]
+	
+[print(x.header["EXTNAME"]) for x in one_hdu_list]
+[print(x.header["EXTNAME"]) for x in two_hdu_list]
